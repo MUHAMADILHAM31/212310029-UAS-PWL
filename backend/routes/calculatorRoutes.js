0bca6c39815e3ca98084fa78bb337060b0671add
MUHAMADILHAM31 <113684047+MUHAMADILHAM31@users.noreply.github.com>
2024-07-13 11:49:49 +0700

    Delete backend directory

--- backend/routes/calculatorRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { hitungHarga } = require('../controllers/calculatorController');
-
-// Endpoint untuk menghitung harga
-router.post('/hitung_harga', hitungHarga);
-
-module.exports = router;
